1ad41234a8b34e0bced87494548fee58bf1adfe8
Ajit Singh <ajit.singh@ecs.co.uk>
2020-09-24 03:27:18 +0530

    progress

--- src/elements/pixi-pg.tsx
+++ src/elements/pixi-pg.tsx
@@ -3,16 +3,25 @@ import { useApp, Stage, Sprite } from '@inlet/react-pixi'
 
 import { FC, WC } from '@/common/types'
 
-export const PixiPg: FC<WC> = () => {
+const Canvas: FC = () => {
+  const app = useApp()
+
   return (
-    <Stage width={480} height={320} options={{ backgroundColor: 0xeef1f5 }}>
+    <>
       <Sprite
         image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/coin.png"
         scale={{ x: 0.5, y: 0.5 }}
         anchor={0.5}
-        x={150}
-        y={150}
+        x={app.renderer.width / 2}
+        y={app.renderer.height / 2}
       />
-    </Stage>
+    </>
   )
 }
+
+// FIXME type of next/dynamic using global override
+export const PixiPg: FC<WC> = () => (
+  <Stage width={480} height={320} options={{ backgroundColor: 0xeef1f5 }}>
+    <Canvas />
+  </Stage>
+)
