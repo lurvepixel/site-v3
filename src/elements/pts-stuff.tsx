01571f58a3d9b8538b05df62bcfa7764d4edf990
Ajit Singh <ajit.singh@ecs.co.uk>
2020-09-26 04:51:46 +0530

    stuff

--- src/elements/pts-stuff.tsx
+++ src/elements/pts-stuff.tsx
@@ -1,5 +1,5 @@
 import { theme } from 'twin.macro'
-import { CanvasSpace, Num } from 'pts'
+import { CanvasSpace, Num, Rectangle, Triangle } from 'pts'
 
 import { useEffect, useRef } from 'react'
 
@@ -16,7 +16,25 @@ export const PtsStuff = () => {
       .play()
     const form = space.getForm()
 
-    space.add(() => form.point(space.pointer, 10))
+    space.add((time, ftime) => {
+      if (time == null) return
+      const rect = Rectangle.fromCenter(space.center, space.size.$divide(2))
+      const poly = Rectangle.corners(rect)
+      poly.shear2D(Num.cycle((time % 5000) / 5000) - 0.5, space.center)
+
+      // triangle
+      const tris = poly.segments(2, 1, true)
+      tris.map(t => t.push(space.pointer))
+
+      // circ
+      const circles = tris.map(t => Triangle.incircle(t))
+
+      // drawing
+      form.fillOnly('#123').polygon(poly)
+      form.fill('#f03').circles(circles)
+      form.strokeOnly('#fff ', 3).polygons(tris)
+      form.fill('#123').point(space.pointer, 5)
+    })
 
     return () => {
       space.dispose()
