d9608535eecc6e5efb67de346ed2e9a004448a47
Ajit Singh <ajit.singh@ecs.co.uk>
2020-09-26 23:31:00 +0530

    show lang in code snippet

--- src/elements/code-block/code-block.tsx
+++ src/elements/code-block/code-block.tsx
@@ -21,26 +21,39 @@ export const CodeBlock: FC<WC<{ className?: string }>> = ({ children, className 
       language={lang as Language}
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre
-          css={css`
-            ${tw`block overflow-auto rounded px-5 py-3 border`}
-            ${font.mono}
-            max-width: 768px;
-          `}
-          className={className}
-          style={{
-            ...style,
-            padding: '20px',
-          }}
-        >
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
-              ))}
+        <div>
+          <div tw="flex justify-end">
+            <div
+              css={css`
+                ${font.monoBold}
+                ${tw`inline-block text-sm border border-b-0 text-sky-gray-500 text-right px-3 py-1 rounded leading-none mr-4`}
+                background-color: ${lightsyntaxTheme.plain.backgroundColor}
+              `}
+            >
+              {lang.toUpperCase()}
             </div>
-          ))}
-        </pre>
+          </div>
+          <pre
+            css={css`
+              ${tw`block overflow-auto rounded px-5 py-3 border`}
+              ${font.mono}
+              max-width: 768px;
+            `}
+            className={className}
+            style={{
+              ...style,
+              padding: '20px',
+            }}
+          >
+            {tokens.map((line, i) => (
+              <div key={i} {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        </div>
       )}
     </Highlight>
   )
