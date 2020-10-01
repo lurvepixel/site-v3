e068fe4be1ba13868f24a72973b73ddf2075e6fb
Ajit Singh <ajit.singh@ecs.co.uk>
2020-10-02 01:05:38 +0530

    react-spring still needs to be patched

--- src/elements/page-content/experiments/z-content.tsx
+++ src/elements/page-content/experiments/z-content.tsx
@@ -1,32 +1,54 @@
 import { css } from 'twin.macro'
 import { useEffect, useRef, useState } from 'react'
-import { CanvasSpace } from 'pts'
+import { CanvasSpace, Group, Pt } from 'pts'
+import { useSpring } from 'react-spring'
 
 import { FC } from '~/shared/types'
 
 export const ZContent: FC = () => {
+  // setup
   const canvasContainerRef = useRef<HTMLDivElement>(null)
+  const spaceRef = useRef<CanvasSpace | null>(null)
 
   useEffect(() => {
     const canvasContainer = canvasContainerRef.current
     if (canvasContainer == null) return
 
     const space = new CanvasSpace(canvasContainer)
-    const form = space.getForm()
-
-    space.add((t, td) => {
-      form.fillOnly('#ee1').rect(space.innerBound)
-    })
-
-    space
       // .bindMouse().bindTouch()
       .play()
+
+    spaceRef.current = space
 
     return () => {
       space.dispose()
       canvasContainer.innerHTML = ''
     }
   }, [])
+
+  // animate stuff
+  const [{ n }] = useSpring(() => ({
+    from: {
+      n: 30,
+    },
+    n: 80,
+    loop: { reverse: true },
+  }))
+
+  useEffect(() => {
+    const space = spaceRef.current
+
+    if (space == null) return
+
+    const form = space.getForm()
+
+    space.add((t, td) => {
+      form.fillOnly('#ee1').rect([
+        [space.center.x - n.get(), space.center.y - n.get()],
+        [space.center.x + n.get(), space.center.y + n.get()],
+      ])
+    })
+  }, [n])
 
   return (
     <div
