e3e0b83d2e41e4b6e14199d657aead9ac5cf93c2
Ajit Singh <ajit.singh@ecs.co.uk>
2020-10-17 16:04:00 +0530

    revert back to package lock

--- src/elements/atoms/button.tsx
+++ src/elements/atoms/button.tsx
@@ -12,13 +12,13 @@ interface ButtonProps {
 export const Button = styled.button<ButtonProps>(
   ({ fullWidth = false, kind = 'default' }) => {
     const kindMap: Record<ButtonKind, any> = {
-      default: tw`text-sky-gray-700 bg-gray-100  border-gray-300`,
+      default: tw`text-sky-gray-700 bg-gray-100 border-gray-400`,
       accent: tw`bg-sky-blue-700 text-white border-sky-blue-700`,
     }
 
     return [
       font.monoBold,
-      tw`text-sm px-4 py-2 border rounded-full tracking-wide`,
+      tw`text-sm px-4 py-2 border rounded-2xl tracking-wide`,
       fullWidth && tw`w-full`,
       kindMap[kind],
     ]
--- src/elements/atoms/headings.tsx
+++ src/elements/atoms/headings.tsx
@@ -4,6 +4,6 @@ import { font } from '~/styles'
 
 export const Heading = {
   H1: styled.h1(tw`text-sky-gray-700 text-5xl tracking-tight leading-tight`, font.display),
-  H2: styled.h2(tw`text-sky-gray-700 text-3xl leading-snug`, font.monoBold),
-  H3: styled.h3(tw`text-sky-gray-700 text-2xl leading-snug`, font.monoBold),
+  H2: styled.h2(tw`text-sky-gray-700 text-4xl leading-snug`, font.serifBold),
+  H3: styled.h3(tw`text-sky-gray-700 text-3xl leading-snug`, font.serifBold),
 }
